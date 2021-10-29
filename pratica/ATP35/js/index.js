93525ee62f3eef3593b385887ef1f977e9019e10
Talita Rosa <talisrosa4@gmail.com>
2021-10-29 17:47:57 -0300

    add ATP35 - Html, Css and Js

---
+++ pratica/ATP35/js/index.js
@@ -0,0 +1,16 @@
+function abstract(){
+    let frame = document.getElementById("frame");
+    frame.src="abstract.html"
+}
+function experience(){
+    let frame = document.getElementById("frame");
+    frame.src="experience.html"
+}
+function ability(){
+    let frame = document.getElementById("frame");
+    frame.src="ability.html"
+}
+function contact(){
+    let frame = document.getElementById("frame");
+    frame.src="contact.html"
+}
